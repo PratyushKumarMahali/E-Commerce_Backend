e1bd42050a3d7b102e19c1a1d9139d5e251515f1
Pratyush Kumar Mahali <83828190+PratyushKumarMahali@users.noreply.github.com>
2022-07-20 18:00:55 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
-from routers import user, image, login, useraddress, category, product, feedback, wishlist, cart
+from routers import user, userimage, login, useraddress, category, product, productimage, feedback, wishlist, cart
 
 app = FastAPI()
 
 app.include_router(user.router)
-app.include_router(image.router)
+app.include_router(userimage.router)
 app.include_router(login.router)
 app.include_router(useraddress.router)
 app.include_router(category.router)
 app.include_router(product.router)
+app.include_router(productimage.router)
 app.include_router(feedback.router)
 app.include_router(wishlist.router)
 app.include_router(cart.router)
